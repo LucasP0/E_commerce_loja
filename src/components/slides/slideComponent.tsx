921fdf67e473a91b8af40cca2429923e0ea42d61
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-08-13 17:29:55 -0300

    ok_ok

--- src/components/slides/slideComponent.tsx
+++ src/components/slides/slideComponent.tsx
@@ -1,13 +1,31 @@
+// IMPORTAÇÕES DO SWIPER:
 import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination, Navigation} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { SliderProducts } from '../../data/products.js';
+
 
 
 export const SlideComponent = () => {
   return (
     <div className="s-containe ">
-      <Swiper>
-        <SwiperSlide>1</SwiperSlide>
-        <SwiperSlide>2</SwiperSlide>
-        <SwiperSlide>3</SwiperSlide>
+      <Swiper
+        sidesPerView={3}
+        spaceBetween={40}
+        slidesPerGroup={1}
+        loop={true}
+      >
+        
+          {SliderProducts.map((slide, i) => (
+            <SwiperSlide>
+              <div className="left-s">
+                
+              </div>
+            </SwiperSlide>
+          ))}
+        
       </Swiper>
     </div>
   )
