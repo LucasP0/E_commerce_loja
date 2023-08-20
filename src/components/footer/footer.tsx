e3d0e5941f04b001175fa8755d43f8c5370621e5
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-08-20 17:07:37 -0300

    KKKKKKKK

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,12 @@
-import { LogIn, Mail, MapPin, Smartphone } from 'lucide-react'
+import { Link, LogIn, Mail, MapPin, Smartphone, Users } from 'lucide-react'
 import Logo from '../../assets/logo.png'
 import { FooterComponent } from './footerComponent'
 
 export const Footer = () => {
   return (
-    <div className='cFooterWrap w-full mt-20'>
+    <div className='cFooterWrap w-full mt-20 '>
       <hr className='w-full  border-white mt-4' />
-      <div className='cfooter flex w-full justify-around'>
+      <div className='cfooter flex w-full justify-around p-6 '>
         <div className='logo flex items-start gap-4'>
           <img
             src={Logo}
@@ -31,6 +31,7 @@ export const Footer = () => {
             spanTwo='suport@amazon.com'
           />
         </div>
+        {/* sign in */}
         <div className='bloc flex flex-col gap-4 max-w-[12rem]'>
           <h1>Conta</h1>
           <FooterComponent
@@ -38,7 +39,27 @@ export const Footer = () => {
             spanTwo='Entrar'
           />
         </div>
+        {/*COMPANHIA  */}
+        <div className='bloc flex flex-col gap-4 max-w-[12rem]'>
+          <h1>Companhia</h1>
+          <FooterComponent
+            icon={<Users />}
+            spanTwo='Sobre Nós'
+          />
+        </div>
 
+        {/*Recursos  */}
+        <div className='bloc flex flex-col gap-4 max-w-[12rem]'>
+          <h1>Recursos</h1>
+          <FooterComponent
+            icon={<Link />}
+            spanTwo='Sobre Nós'
+          />
+        </div>
+      </div>
+      <div className='flex flex-col w-full text-left text-[0.70rem] mt-8 p-2'>
+        <span>Copyright @2022 by Amazon, Inc.</span>
+        <span>All rights reserved.</span>
       </div>
     </div>
   )
