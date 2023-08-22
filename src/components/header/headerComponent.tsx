47a847d531e668e978000c02d36ed7fe45a9e7de
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-08-21 22:25:35 -0300

    ok

--- src/components/header/headerComponent.tsx
+++ src/components/header/headerComponent.tsx
@@ -1,31 +1,43 @@
-import { ShoppingCart } from 'lucide-react';
+import { Menu, ShoppingCart } from 'lucide-react';
 import logo from '../../assets/logo.png';
+import { useState } from 'react';
 
 export const HeaderComponent = () => {
+  const [showMenu, setShowMenu] = useState
+    (true);
+  const handleShowMenu = () => {
+    setShowMenu((showMenu) => !showMenu)
+  }
+
+
   return (
-    <div className="Container p-6 flex flex-row items-center">
+    <div className="conta---   p-6 flex flex-row items-center">
       <div className="Logo flex flex-row flex-1 items-center gap-2">
         <img
           className='w-10 h-10'
           src={logo} alt="" />
-          <span className='font-semibold'>Amazon</span>
+        <span className='font-semibold'>Amazon</span>
       </div>
-      <div className="right flex flex-row gap-4 items-center">
-        <div className="menu">
-          <ul className='flex flex-row gap-4 text-lg font-semibold'>
-            <li>Coleção</li>
-            <li>Marcas</li>
-            <li>Novo</li>
-            <li>Promoções</li>
-            <li>Eng</li>
-          </ul>
+
+      <div className="max-right flex flex-row gap-4 items-center  ">
+        <div className='bars max-sm:block' onClick={handleShowMenu}>
+          <Menu />
         </div>
-        <input 
+
+        <ul className='menu flex flex-row gap-4 text-lg font-semibold max-sm:text-center'
+          style={{ display: showMenu ? 'inherit' : 'none' }}>
+          <li>Coleção</li>
+          <li>Marcas</li>
+          <li>Novo</li>
+          <li>Promoções</li>
+          <li>Eng</li>
+        </ul>
+        <input
           type="search"
           placeholder='Search...'
-          className="search h-[30%] border-2 outline-none p-2 border-black" 
+          className="search h-[30%] border-2 outline-none max-sm:w-[150px] p-2 border-black max-sm:hidden"
         />
-        <ShoppingCart size={30}/>
+        <ShoppingCart size={30} />
       </div>
     </div>
   )
