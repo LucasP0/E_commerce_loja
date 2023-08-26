6a760b8a77c51f25ccd6c1afd5373350fd4556e7
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-08-26 00:43:36 -0300

    Atualização

--- src/components/logIn/logComponent.tsx
+++ src/components/logIn/logComponent.tsx
@@ -2,10 +2,18 @@ import { KeyRound, Mail } from "lucide-react"
 import { useState } from "react";
 
 export const LogComponent = () => {
-
+  const [input, setInput] = useState('password');
+  const handleChange = () => {
+    if (input === 'password') {
+      setInput('type')
+    } else {
+      setInput('password')
+    }
+  }
   return (
     <div className="flex items-center justify-center">
-      <div className="bg-white w-[1000px] h-[650px] rounded-md flex flex-col justify-center items-center gap-20">
+      <div 
+      className="bg-white w-[1000px] h-[650px] rounded-md flex flex-col justify-center items-center gap-20">
         <section className="flex flex-col items-center gap-10 ">
           <h1 className="text-[2rem] font-semibold">Login</h1>
           <div className="flex flex-col items-start w-[300px] gap-2 ">
@@ -13,7 +21,7 @@ export const LogComponent = () => {
               <Mail size={20} /> E-mail</div>
             <input
               type="e-mail"
-              className="w-[300px] border-2 border-blue-800 rounded-md h-10 p-2 outline-none" placeholder="E-mail" onSubmit={''}
+              className="w-[300px] border-2 border-blue-800 rounded-md h-10 p-2 outline-none" placeholder="E-mail"
             />
             <div className="flex flex-row gap-2 items-center">
               <KeyRound size={20} />
@@ -21,9 +29,10 @@ export const LogComponent = () => {
             </div>
             <div className="">
               <input
-              type="password"
-                className="w-[300px] border-2 border-blue-800 rounded-md h-10 p-2 outline-none" placeholder="Senha" onSubmit={''}
+                type={input}
+                className="w-[300px] border-2 border-blue-800 rounded-md h-10 p-2 outline-none" placeholder="Insira sua senha"
               />
+              <h1 onClick={handleChange}>OK</h1>
             </div>
             <h2 className="text-right w-full text-blue-800 hover:text-gray-600">Esqueci a senha</h2>
             <h2 className="text-right w-full text-blue-800 hover:text-gray-600">Criar Conta</h2>
