f06a4e955a09e46aa2a360c38806c89048d88d8b
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-09-05 20:56:21 -0300

    Atualizando para fixed

--- src/components/cart/cartComponent.tsx
+++ src/components/cart/cartComponent.tsx
@@ -1,14 +1,13 @@
 import { CartItem } from "./Cartitem"
-import img1 from '../../assets/before.png'; 
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import formatCurrency from "./formatCurrency";
 
 export const CartComponent = () => {
-  const { cartItems, isCartVisible } = useContext(AppContext);
+  const { cartItems, isCartVisible } = useContext<any>(AppContext);
   const totalPrice = cartItems.reduce((acc,item) => item.price + acc, 0);
   return (
-    <section className={`cart w-full max-w-[300px] bg-white h-[100vh] absolute overflow-y-scroll  flex-col justify-between  top-0 right-0 pt-[100px] hidden pl-2 pr-2 z-30 transform translate-x-[110%]  active:translate-x-0  translate-y-0 transition-all ${isCartVisible ? 'cart--active' : ''}`}>
+    <section className={`cart w-full max-w-[300px] bg-white h-[100vh] fixed overflow-y-scroll  flex-col justify-between  top-0 right-0 pt-[100px] hidden pl-2 pr-2 z-30 transform translate-x-[110%]  active:translate-x-0  translate-y-0 transition-all ${isCartVisible ? 'cart--active' : ''}`}>
       <div>
         { cartItems.map((item) => 
         <CartItem 
--- src/components/header/headerComponent.tsx
+++ src/components/header/headerComponent.tsx
@@ -12,50 +12,44 @@ export const HeaderComponent = () => {
 
 
   return (
-    <div className="conta---   p-6 flex flex-row items-center z-50 relative bg-[#F8E367]">
-      <div className="Logo flex flex-row flex-1 items-center gap-2">
-        <Link to={'/'} className='flex flex-row items-center gap-2'>
-          <img
-            className='w-10 h-10'
-            src={logo} alt="" />
-          <span className='font-semibold'>Amazon</span>
-        </Link>
-      </div>
-
-      <div className="max-right flex flex-row gap-4 items-center ">
-        <div className='bars max-sm:block' onClick={handleShowMenu}>
-          <Menu className='hidden max-sm:flex' />
+      <div className="fixed conta---   p-6 flex flex-row items-center top-0 z-50 bg-[#F8E367] w-full">
+        <div className="Logo flex flex-row flex-1 items-center gap-2">
+          <Link to={'/'} className='flex flex-row items-center gap-2'>
+            <img
+              className='w-10 h-10'
+              src={logo} alt="" />
+            <span className='font-semibold'>Amazon</span>
+          </Link>
         </div>
-
-        <ul className='menu flex flex-row gap-4 text-lg font-semibold max-sm:text-center'
-          style={{ display: showMenu ? 'inherit' : 'none' }}>
-          <a href="#coleções" className='hover:text-white'>
-            <li>Coleções</li>
-          </a>
-          <a href="#destaques" className='hover:text-white'>
-            <li>Destaques</li>
-          </a>
-          <a href="#virtual" className='hover:text-white'>
-            <li>Virtual</li>
-          </a>
-
-        </ul>
-
-
-        <Link to={'/log'}>
-          <div className='text-lg font-semibold flex flex-row items-center gap-2 hover:text-white'>
-            <h2 className='max-sm:hidden'>Log In</h2>
-            <LogIn size={30} />
+        <div className="max-right flex flex-row gap-4 items-center ">
+          <div className='bars max-sm:block' onClick={handleShowMenu}>
+            <Menu className='hidden max-sm:flex' />
           </div>
-        </Link>
-        <input
-          type="search"
-          placeholder='Search...'
-          className="search h-[30%] border-2 outline-none max-sm:w-[150px] p-2 border-black max-sm:hidden max-md:hidden"
-        />
-
-        <EmojiCar />
+          <ul className='menu flex flex-row gap-4 text-lg font-semibold max-sm:text-center'
+            style={{ display: showMenu ? 'inherit' : 'none' }}>
+            <a href="#coleções" className='hover:text-white'>
+              <li>Coleções</li>
+            </a>
+            <a href="#destaques" className='hover:text-white'>
+              <li>Destaques</li>
+            </a>
+            <a href="#virtual" className='hover:text-white'>
+              <li>Virtual</li>
+            </a>
+          </ul>
+          <Link to={'/log'}>
+            <div className='text-lg font-semibold flex flex-row items-center gap-2 hover:text-white'>
+              <h2 className='max-sm:hidden'>Log In</h2>
+              <LogIn size={30} />
+            </div>
+          </Link>
+          <input
+            type="search"
+            placeholder='Search...'
+            className="search h-[30%] border-2 outline-none max-sm:w-[150px] p-2 border-black max-sm:hidden max-md:hidden"
+          />
+          <EmojiCar />
+        </div>
       </div>
-    </div>
   )
 }
